c74f8b00385ef4f293b72efb76712a09d8d3dc43
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-02-04 16:17:26 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX=0;
+noseY=0;
+
 function preload()
 {
 }
@@ -16,6 +19,9 @@ function setup(){
 function draw()
 {
     image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(noseX, noseY, 20);
 }
 
 function take_snapshot()
@@ -33,7 +39,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
